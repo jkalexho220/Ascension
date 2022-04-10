12938a07ce935328174aad25f9dcd0537353ffac
Alex Ho <jkalexho220@gmail.com>
2022-04-09 22:09:42 -0400

    rmsify ignores single-line comments too

--- rmsify.py
+++ rmsify.py
@@ -1095,7 +1095,7 @@ try:
 									print("Missing semicolon")
 									print("Line " + str(ln) + ":\n    " + line)
 
-								if not RESTORING:
+								if not RESTORING and len(templine) > 0:
 									# reWrite the line
 									if first or ESCAPE:
 										file_data_2.write(templine + '\n')
